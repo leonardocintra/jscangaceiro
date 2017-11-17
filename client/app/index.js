74a985883efe6a3685990c09d7416003661341b3
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-11-17 19:43:44 -0200

    exibindo e tratando basicamente

--- client/app/index.js
+++ client/app/index.js
@@ -9,6 +9,7 @@ console.log(campos);
 var tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault();    
     var tr = document.createElement('tr');
 
     campos.forEach(function(campo) {
@@ -17,5 +18,15 @@ document.querySelector('.form').addEventListener('submit', function(event) {
         td.textContent = campo.value;
         tr.appendChild(td);
     });
-    // parou na 34
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+    campos[0].value = ""
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
 });
