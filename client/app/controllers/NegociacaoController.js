9546e61b95d27821bedfc774fd5d2c065c774ed0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-11-18 14:43:08 -0200

    parei em 99

---
+++ client/app/controllers/NegociacaoController.js
@@ -0,0 +1,18 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+    
+    adiciona(event) {
+        event.preventDefault();
+
+        console.log(this._inputData.value);
+        console.log(parseInt(this._inputQuantidade.value));
+        console.log(parseFloat(this._inputValor.value));
+    }
+    // parei em 99
+}
