228bcc2bf1f3261505ef830181428cf14a18e7cf
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-03-21 20:49:48 -0300

    bug fix

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,20 +6,22 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._negociacoes = new Negociacoes();
-        this._negociacoesView = new NegociacoesView();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update(this._negociacoes);
     }
     
     adiciona(event) {
         event.preventDefault();
 
         this._negociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._negociacoes);
         this._limpaFormulario();
     }
 
     _limpaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
-        this._inputValor = 0;
+        this._inputValor.value = 0;
         this._inputData.focus();
     }
 
--- client/app/ui/views/NegociacoesView.js
+++ client/app/ui/views/NegociacoesView.js
@@ -1,6 +1,14 @@
 class NegociacoesView {
-  
-  template(){
+
+  constructor(seletor) {
+    this._elemento = document.querySelector(seletor);
+  }
+
+  update(model) {
+    this._elemento.innerHTML = this.template(model);
+  }
+
+  template(model){
     return `
     <table class="table table-hover table-bordered">
         <thead>
@@ -13,9 +21,22 @@ class NegociacoesView {
         </thead>
         
         <tbody>
+          ${model.paraArray().map(negociacao => {
+            return `
+            <tr>
+              <td>${DateConverter.paraTexto(negociacao.data)}</td>
+              <td>${negociacao.quantidade}</td>
+              <td>${negociacao.valor}</td>
+              <td>${negociacao.volume}</td>
+            </tr>`
+          }).join('')}
         </tbody>
         
         <tfoot>
+          <tr>
+            <td colspan="3"></td>
+            <td>${model.volumeTotal}</td>
+          </tr>
         </tfoot>
     </table>
     `
